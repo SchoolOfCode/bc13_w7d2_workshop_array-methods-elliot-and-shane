c5190a16f757c630ced4ca440ac0e8b6ecc18c51
elliot tasker <elliottasker@gmail.com>
2022-11-08 12:18:17 +0000

    task 2 complete

--- task2.js
+++ task2.js
@@ -8,4 +8,8 @@ let animals = [
     "basalisk",
   ];
   
-let bListAnimals = // Your 'filter' code here.
+let bListAnimals = animals.filter(function(word){
+  return word[0] === 'b'
+})
+
+console.log(bListAnimals)
