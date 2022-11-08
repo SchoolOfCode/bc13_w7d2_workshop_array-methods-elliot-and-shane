74448ff99aee40876b5b50cadc1df48b78a5bc9a
shanedownes <shanedownes01@gmail.com>
2022-11-08 13:00:21 +0000

    Task 3 array solution

--- task3.js
+++ task3.js
@@ -1,4 +1,6 @@
 let someNumbers = [4, 5, 6, 78, 2, 3, 45, 34, 2, 23, 5, 45, 6, 7, 23];
+
+// multiple variables solution
 let n3 = 3;
 let n5 = 5;
 let n60 = 60;
@@ -13,3 +15,12 @@ console.log(n3);
 console.log(n5);
 console.log(n60);
 console.log(n90);
+
+
+// array solution
+let multiplesOf = [3, 5, 60, 90];
+
+let multiples = multiplesOf.forEach((num) => {
+    let result = someNumbers.some(n => n % num === 0);
+    console.log(result);
+});
