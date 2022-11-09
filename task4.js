64af3dc295300a557a5a8b0f9c59cf9d44fb9787
shanedownes <shanedownes01@gmail.com>
2022-11-09 20:44:59 +0000

    Task 4

--- task4.js
+++ task4.js
@@ -1,3 +1,10 @@
 let sevenTimesTable = [7, 14, 21, 28, 35, 42, 49, 56, 63, 70];
 
-let seventySevenTimesTable = [77, 154, 231, 308, 385, 461, 538, 616, 693, 770];
+let seventySevenTimesTable = [77, 154, 231, 308, 385, 462, 539, 616, 693, 770];
+
+
+console.log(seventySevenTimesTable.every((e, i) => e === 11*sevenTimesTable[i]));
+
+console.log(seventySevenTimesTable.find((e, i) => e != 11*sevenTimesTable[i]));
+
+console.log(seventySevenTimesTable.findIndex((e, i) => e != 11*sevenTimesTable[i]));
