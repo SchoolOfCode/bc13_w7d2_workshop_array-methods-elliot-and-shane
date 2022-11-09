a20b8994989d98ca5cd6b3a0248d90b9898261de
shanedownes <shanedownes01@gmail.com>
2022-11-09 21:06:46 +0000

    Task 5

--- task5.js
+++ task5.js
@@ -9,3 +9,20 @@ let queue = [
   "catHorse",
   "dogPig",
 ];
+
+const unofficialQueue = [
+  "rabbitSalmon",
+  "rabbitSalmon",
+  "rabbitSalmon",
+  "rabbitSalmon",
+];
+
+queue.push("catHorse");
+queue.shift(0);
+queue.unshift("owlDoknkey");
+queue.pop();
+queue.splice(4, 1);
+console.log(queue);
+console.log(queue.concat(unofficialQueue));
+
+
